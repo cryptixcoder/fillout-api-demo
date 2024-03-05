83e465635f1cfe2bbad37f386a46f9dac06d6443
Markus Gray <syncwarellc@gmail.com>
2024-03-05 09:12:08 -0500

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import routes from './routes'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', routes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+export default app
