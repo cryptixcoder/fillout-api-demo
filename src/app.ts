edc3e03377cb941f5cab117cc6d75ceeaa7acd09
Markus Gray <syncwarellc@gmail.com>
2024-03-05 12:30:04 -0500

    feat: added a frontend react app for testing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 
 import express from "express";
+import path from "path";
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes'
@@ -11,7 +12,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use('/api', routes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
