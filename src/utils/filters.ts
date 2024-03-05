a6303faca17b6249f473f2a5ca4168e7df5242b7
Markus Gray <syncwarellc@gmail.com>
2024-03-05 16:25:32 -0500

    fix: refactored applyFilters to take into account the difference between NumberInput and DatePicker

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -9,8 +9,22 @@ export const applyFilters = (responses: any[], filters: ResponseFilterType) => {
             switch (filter.condition) {
                 case 'equals': return question.value === filter.value;
                 case 'does_not_equal': return question.value !== filter.value;
-                case 'greater_than': return new Date(question.value) > new Date(filter.value);
-                case 'less_than': return new Date(question.value) < new Date(filter.value);
+                case 'greater_than': 
+                    if (question.type === 'DatePicker') {
+                        return new Date(question.value) > new Date(filter.value);
+                    } else if (question.type === 'NumberInput') {
+                        // Ensure both values are treated as numbers
+                        return Number(question.value) > Number(filter.value);
+                    }
+                    break;
+                case 'less_than': 
+                    if (question.type === 'DatePicker') {
+                        return new Date(question.value) < new Date(filter.value);
+                    } else if (question.type === 'NumberInput') {
+                        // Ensure both values are treated as numbers
+                        return Number(question.value) < Number(filter.value);
+                    }
+                    break;
                 default: return true;
             }
         });
