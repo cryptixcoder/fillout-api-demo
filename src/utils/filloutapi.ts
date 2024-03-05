a9acfc1e1b9081d02fd741ec05a1a0bc45699a1d
Markus Gray <syncwarellc@gmail.com>
2024-03-05 09:35:22 -0500

    feat: setup fillout api client

---
+++ src/utils/filloutapi.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const token = process.env.FILLOUT_KEY;
+
+if (!token) {
+    console.log("FILLOUT_KEY is missing from env");
+    process.exit(1);
+}
+
+const client = axios.create({
+    baseURL: "https://api.fillout.com/v1",
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+client.interceptors.request.use((config) => {
+    if (config.headers && token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+},(error) => {
+    return Promise.reject(error);
+});
+
+export default client;
